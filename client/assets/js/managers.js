57dc25599ececd72618387cb0c0610cc0dd58350
Jacob Smith <messagethesmith@gmail.com>
2017-04-02 14:49:46 -0700

    Try different require syntax in managers.js

--- client/assets/js/managers.js
+++ client/assets/js/managers.js
@@ -1,15 +1,8 @@
 const RubricLib = require('rubricjs');
-const {
-    EntityManager,
-    EntityFactory,
-    SystemManager,
-    // TimerManager
-} = RubricLib;
+const {EntityManager, EntityFactory, SystemManager} = RubricLib;
 
 module.exports = {
     entityManager: new EntityManager(),
     entityFactory: new EntityFactory(),
     systemManager: new SystemManager(),
-    // Unnecessary for our demo
-    // timerManager: new TimerManager(),
 };
